350f005bd654cd86e1c2397ff7e8210a4c5fc959
Rui Tu <ruit@uoregon.edu>
2016-10-13 09:22:02 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ git clone https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 
 cd ..
-mv my_configs ~/.vim_runtime
+mv ~/my_configs ~/.vim_runtime
 echo 'source ~/.vim_runtime/my_configs/my_configs.vim' > ~/.vimrc
 
 echo "My personal config is successfully installed!"
