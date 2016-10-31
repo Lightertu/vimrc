fd60a3192acb98ff72ccc732f304dc730c33deb7
lightertu <ruit@uoregon.edu>
2016-10-30 21:13:56 -0700

    dded platform config

--- install_ycm.sh
+++ install_ycm.sh
@@ -1,6 +1,18 @@
 #!bin/bash
+
+unamestr=`uname`
+
+# ubuntu platform
+if [[ "$unamestr" == 'Linux' ]]; then
+   sudo apt-get install build-essential cmake
+   sudo apt-get install python-dev python3-dev
+elif [[ "$unamestr" == 'Darwin' ]]; then
+   brew install cmake
+   clang
+fi
+
 git clone https://github.com/Valloric/YouCompleteMe.git  ~/.vim_runtime/sources_non_forked/YouCompleteMe
 cd ~/.vim_runtime/sources_non_forked/YouCompleteMe/
 
 git submodule update --init --recursive 
-./install.py
+./install.py --all
